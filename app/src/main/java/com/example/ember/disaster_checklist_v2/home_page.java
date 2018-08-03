716d93a0d18db9925903d448a4fb186f814a36b1
Amber Dickey <ard0046@auburn.edu>
2018-08-03 16:35:24 -0500

    Menu is working :)

---
+++ app/src/main/java/com/example/ember/disaster_checklist_v2/home_page.java
@@ -0,0 +1,14 @@
+package com.example.ember.disaster_checklist_v2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class home_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
---
+++ app/src/main/java/com/example/ember/disaster_checklist_v2/menu_main.java
@@ -0,0 +1,39 @@
+package com.example.ember.disaster_checklist_v2;
+
+import android.support.annotation.MenuRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class menu_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.checklist:
+                Toast.makeText(this, "Checklist",Toast.LENGTH_LONG).show();
+                return true;
+                }
+        return true;
+    }
+}
