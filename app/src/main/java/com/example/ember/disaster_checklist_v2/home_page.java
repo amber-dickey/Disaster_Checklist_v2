af2e5ca0a612756ed87568e141cf917760f76360
Amber Dickey <ard0046@auburn.edu>
2018-08-06 22:30:03 -0500

    tried putting the menu main in list

--- app/src/main/java/com/example/ember/disaster_checklist_v2/home_page.java
+++ app/src/main/java/com/example/ember/disaster_checklist_v2/home_page.java
@@ -1,14 +1,30 @@
 package com.example.ember.disaster_checklist_v2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class home_page extends AppCompatActivity {
-
+    private static final String TAG = "home_page";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Log.d(TAG, "onCreate: Starting.");
+        Button checklist = (Button) findViewById(R.id.checklist_button);
+        checklist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: CLicked Checklist.");
+                Intent intent = new Intent(home_page.this, menu_main.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
 }
--- app/src/main/java/com/example/ember/disaster_checklist_v2/list_checklist.java
+++ app/src/main/java/com/example/ember/disaster_checklist_v2/list_checklist.java
@@ -1,46 +1,43 @@
 package com.example.ember.disaster_checklist_v2;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-
-public class menu_main extends AppCompatActivity {
+public class list_checklist extends AppCompatActivity{
     TextView waterValue;
     int waterCounter = 0;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
-
+        setContentView(R.layout.layout_listitem);
         waterValue = (TextView) findViewById(R.id.waterValue);
+
     }
-    //creates the options menu
+  /*  //creates the options menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
-    }
+    }*/
 
-    //shows the checklist menu options
+ /*   //shows the checklist menu options
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         super.onOptionsItemSelected(item);
         switch (item.getItemId()) {
             case R.id.checklist:
-               setContentView(R.layout.layout_listitem);
-               return true;
-               //original test to see if method worked
-                /*Toast.makeText(this, "Checklist", Toast.LENGTH_LONG).show();
-                return true;*/
-                }
+                setContentView(R.layout.layout_listitem);
+                return true;
+            //original test to see if method worked
+                *//*Toast.makeText(this, "Checklist", Toast.LENGTH_LONG).show();
+                return true;*//*
+        }
         return true;
-    }
+    }*/
 
     //increases the click of the water button
     public void increaseButton(View view) {
